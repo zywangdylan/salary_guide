f1e0951b0f4769519aa6fe75ad7cae7ddc2b28d3
Pavel Liser <liser.pavel@gmail.com>
2020-05-15 15:59:45 +0800

    added a hash of all job categories, now filtering by city

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const BaaS = window.BaaS
 BaaS.init('69971365ede01a812c8a')
 const SectorsMap = {
-  "Data and AI": "data-ai-jobs"
+  "Data and AI": "data-ai-jobs",
+  "Web Building": "web-building-jobs",
+  "General IT & Project Management": "it-general-pm-jobs",
+  "IT Security": "security-jobs",
+  "IT Development & Architecture": "dev-architecture-jobs",
+  "IT Testing": "testing-jobs",
+  "Solution Consulting": "solution-consulting-jobs",
+  "Design": "design-jobs",
+  "Digital Strategy": "digital-strategy-jobs",
+  "Product & Operations": "product-ops-jobs"
 }
 
 async function getData() {
@@ -23,12 +32,15 @@ let Submissions = new BaaS.TableObject('submissions')
   let record = await getRecord()
   let entry = record.data
   if (entry) {
-    entry.sectors.forEach(s => {
-      table = document.querySelector(`#${SectorsMap[s]}`)
-      console.log(table)
+    console.log(entry)
+    entry.cities.forEach(city => {
+      entry.sectors.forEach(s => {
+      table = document.querySelector(`.${city} #${SectorsMap[s]}`)
+      console.log(s)
       if (table) {
         table.hidden = false
       }
+      })
     })
   } else {
     getData()
