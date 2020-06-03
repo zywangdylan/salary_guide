c3a593f69ba281a4230aa01305044a52b39ac027
Pavel Liser <liser.pavel@gmail.com>
2020-06-03 18:40:05 +0800

    changed logic to receive a code by sms and put apologies for blank data

--- index.js
+++ index.js
@@ -13,45 +13,66 @@ const SectorsMap = {
   "Product & Operations": "product-ops-jobs"
 }
 
-async function getData() {
-let Submissions = new BaaS.TableObject('submissions')
+async function getData(entry_id) {
+  document.querySelector('.code').hidden = true
+  document.querySelector('.spinner-border').hidden = false
+  let Submissions = new BaaS.TableObject('submissions')
 
   async function getRecord() {
     try {
-      let queryString = window.location.search
-      let entry_id = new URLSearchParams(queryString).get('id')
+      // let queryString = window.location.search
+      // let entry_id = new URLSearchParams(queryString).get('id')
       let res = await Submissions.get(entry_id)
       // success
       return res
     } catch(err) {
       // error
-      throw err
+      return err
     }
   }
 
   let record = await getRecord()
-  let entry = record.data
-  if (entry) {
-    console.log(entry)
-    document.querySelector('#user-name').innerText = entry.name
-    entry.cities.forEach(city => {
-      document.querySelector(`#${city.toLowerCase()}`).hidden = false
-      entry.sectors.forEach(s => {
-      table = document.querySelector(`.${city} #${SectorsMap[s]}`)
-      if (table) {
-        table.hidden = false
-      }
-      })
-    })
+  if (record.status != 200) {
+    document.querySelector('input').value = ''
+    document.querySelector('.code').hidden = false
+    document.querySelector('.spinner-border').hidden = true
+    document.querySelector('.oops').hidden = false
   } else {
-    getData()
+    let entry = record.data
+    if (entry) {
+      console.log(entry)
+      document.querySelector('.intro').hidden = false
+      document.querySelector('.spinner-border').hidden = true
+      document.querySelector('.header').style.height = 'auto'
+      document.querySelector('#user-name').innerText = entry.name
+      entry.cities.forEach(city => {
+        document.querySelector(`#${city.toLowerCase()}`).hidden = false
+        entry.sectors.forEach(s => {
+          table = document.querySelector(`.${city} #${SectorsMap[s]}`)
+          if (table) {
+            table.hidden = false
+          }
+        })
+      })
+    } else {
+      getData(entry_id)
+    }
   }
 }
 
-getData()
 
+
+// parallax effect
 window.addEventListener('scroll', e => {
   let scrolled = window.pageYOffset
   const bg = document.querySelector('.bg')
-  bg.style.top = - (scrolled * 0.4)
+  bg.style.top = - (scrolled * 0.15)
+})
+
+// code input logic
+const input = document.querySelector('input')
+input.addEventListener('input', e => {
+  if (input.value.length == 24) {
+    getData(input.value)
+  }
 })
